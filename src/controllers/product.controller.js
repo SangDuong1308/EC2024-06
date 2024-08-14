8a190834d159872328b0228fda3b742d8831f05e
Sang Duong <duongphuocsang1308@gmail.com>
2024-08-14 07:04:33 +0700

    fix

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -3,7 +3,7 @@ const { getProductById, getAllProducts } = require("../services/product.service"
 
 module.exports = {
     async getAllProducts(req, res, next) {
-        const { limit = 50, sort = "ctime", page = 1 } = req.params;
+        const { limit = 50, sort = "ctime", page = 1 } = req.query;
         try {
             const filter = { isActive: true };
             const select = [
